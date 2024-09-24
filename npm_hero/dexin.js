67b6d178c06fad4e70cf5fb72a1272e2e8f7502a
AabB1150-hub <655021000600@mail.rmutk.ac.th>
2024-09-24 16:43:50 +0700

    Your commit message

---
+++ npm_hero/dexin.js
@@ -0,0 +1,3 @@
+import 'suppress-experimental-warnings';
+
+await import('./index.js');
---
+++ npm_hero/index.js
@@ -0,0 +1,3 @@
+import {randomSuperhero} from 'superheroes';
+var hero=randomSuperhero();
+console.log(`your name are ${hero}`);
